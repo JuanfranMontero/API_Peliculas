bcfca67621cad134a7f3603504dff65f24eca4fd
JuanfranMontero <juanfransmr@gmail.com>
2022-01-11 21:19:54 +0100

    Add page navigation

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,40 @@
 window.onload = () => {
 
-    const apiKey = '1cc17c9ae3042dcded67807cd410be6f'
+    let page = 1
+    const btnAnterior = document.querySelectorAll('button')[0]
+    const btnSiguiente = document.querySelectorAll('button')[1]
+
+    btnAnterior.addEventListener('click', () => {
+        if(page > 1){
+            page -= 1;
+            showAllMovies()
+        } 
+    });
+
+    btnSiguiente.addEventListener('click', () => {
+        if(page < 1000){
+            page += 1;
+            showAllMovies()
+        } 
+    });
+
     const showAllMovies = async () => {
         try {
-            const reply = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=1cc17c9ae3042dcded67807cd410be6f')
+            const reply = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=1cc17c9ae3042dcded67807cd410be6f&page=${page}`)
             console.log(reply);
             if(reply.status === 200){
                 const dataMovies = await reply.json()
-                
+                let movies = ''
                 dataMovies.results.forEach(movie => {
-                    let article = document.createElement('article')
-                    let imgMovie = document.createElement('img')
-                    imgMovie.src = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`
-                    let titleMovie = document.createElement('h3')
-                    let nodeText = document.createTextNode(movie.title)
-                    titleMovie.appendChild(nodeText)
-                    article.appendChild(imgMovie)
-                    article.appendChild(titleMovie)
-                    article.className = 'peliculas'
-                    document.getElementById('containerMovies').appendChild(article)
+                    movies += `
+                            <article class='peliculas'>
+                                <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}"/>
+                                <h3>${movie.title}</h3>
+                            </article>
+                            `
+                    
                 })
-                
+                document.getElementById('containerMovies').innerHTML = movies
             }
         } 
         catch (error) {
