dbb5af7fbe3aa13f90a90de4f96c5326e749dd05
JuanfranMontero <juanfransmr@gmail.com>
2022-01-30 19:04:11 +0100

    Add page Now Playing and Top Rated

--- assets/main.js
+++ assets/main.js
@@ -18,6 +18,19 @@ window.onload = () => {
         } 
     });
 
+    document.querySelector('#popular').addEventListener('click', () => {
+        page = 1
+        showAllMovies()
+    })
+    document.querySelector('#nowPlaying').addEventListener('click', () => {
+        page = 1
+        nowPLaying()
+    })
+    document.querySelector('#topRated').addEventListener('click', () => {
+        page = 1
+        topRated()
+    })
+
     const showAllMovies = async () => {
         document.getElementById('page').textContent = page
         try {
@@ -43,5 +56,57 @@ window.onload = () => {
         }
     }
     showAllMovies()
+
+
+    const nowPLaying = async () => {
+        document.getElementById('page').textContent = page
+        try {
+            const reply = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=1cc17c9ae3042dcded67807cd410be6f&page=${page}`)
+            console.log(reply);
+            if(reply.status === 200){
+                const dataMovies = await reply.json()
+                let movies = ''
+                dataMovies.results.forEach(movie => {
+                    movies += `
+                            <article class='peliculas'>
+                                <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}"/>
+                                <h3>${movie.title}</h3>
+                            </article>
+                            `
+                    
+                })
+                document.getElementById('containerMovies').innerHTML = movies
+            }
+        } 
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    const topRated = async () => {
+        document.getElementById('page').textContent = page
+        try {
+            const reply = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=1cc17c9ae3042dcded67807cd410be6f&page=${page}`)
+            console.log(reply);
+            if(reply.status === 200){
+                const dataMovies = await reply.json()
+                let movies = ''
+                dataMovies.results.forEach(movie => {
+                    movies += `
+                            <article class='peliculas'>
+                                <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}"/>
+                                <h3>${movie.title}</h3>
+                            </article>
+                            `
+                    
+                })
+                document.getElementById('containerMovies').innerHTML = movies
+            }
+        } 
+        catch (error) {
+            console.log(error);
+        }
+    }
+
 
 }
